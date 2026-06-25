8f06d384cc5bbc29dcfe103f496dc54751b3da9c
Anna Smyrnova <anyto4ka777s@gmail.com>
2026-06-25 21:33:46 +0300

    feat: implement user avatar upload to cloudinary

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import NotesRouter from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
-
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -22,13 +22,13 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-
 // GET-запит до "/notes"
 // GET-запит  "/notes/:noteId"
 app.use(NotesRouter);
 
 app.use(authRoutes);
 
+app.use(userRoutes);
 
 // test-error
 app.get('/test-error', (req, res) => {
